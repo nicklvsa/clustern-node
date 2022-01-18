070cb004a5b6ab9823960afb193b238afd4dcdcd
nicklvsa <nick@charliesdesk.com>
2022-01-17 19:26:08 -0500

    amend script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -10,6 +10,7 @@ chmod 755 /usr/local/bin/pacapt
 ln -sv /usr/local/bin/pacapt /usr/local/bin/pacman || true 
 pacapt update
 pacapt install git
+pacapt install screen
 wget https://golang.org/dl/go1.15.7.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.15.7.linux-amd64.tar.gz
 rm -rf go1.15.7.linux-amd64.tar.gz
@@ -17,4 +18,4 @@ export PATH=$PATH:/usr/local/go/bin
 mkdir app
 git clone https://github.com/nicklvsa/clustern-node
 cd clustern-node
-go run main.go &
+screen go run main.go
